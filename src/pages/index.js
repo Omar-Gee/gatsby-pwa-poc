7e639c504c9820495d5ab1fdb87788565d2e2ba6
Omar <omargeerman@gmail.com>
2020-04-14 19:59:53 +0200

    added env vars

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,23 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <a href="https://d1oq5edtpw689g.cloudfront.net/createEvent">
-      <button>Create event</button>
-    </a>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const href = process.env.GATSBY_EXTERNAL_SITE;
+  return (
+    < Layout >
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <a href={href}>
+        <button>Create event</button>
+      </a>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout >
+  )
+}
 
 export default IndexPage
